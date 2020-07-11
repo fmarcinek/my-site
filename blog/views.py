8975e897e8f658c4b08ea13e031f90246465cc51
Filip Marcinek <filipmar@interia.eu>
2020-07-11 15:03:12 +0200

    Create Post table

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', {'posts': Post.objects.all()})
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
