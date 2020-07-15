34c3c9a388f20acd4a7f72d6374f1704dae6b79b
Filip Marcinek <filipmar@interia.eu>
2020-07-15 20:29:54 +0200

    Enable creating a post by user

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 from .models import Post
 
 
+# not in use now
 def home(request):
     return render(request, 'blog/home.html', {'posts': Post.objects.all()})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
