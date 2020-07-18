38f37a69308ade694432132c085d0d1d3af70547
Filip Marcinek <filipmar@interia.eu>
2020-07-18 13:33:08 +0200

    Update app name

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['apple-challenge.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['super-smart-app.herokuapp.com', 'localhost']
 
 # Application definition
 
